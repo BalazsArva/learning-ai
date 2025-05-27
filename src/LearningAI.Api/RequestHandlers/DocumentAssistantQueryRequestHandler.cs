22cd05fa00ad31d8141991754a139e8063c1e6ca
BalazsArva <balazs.arva@windowslive.com>
2025-05-27 14:16:19 +0200

    Formatting

--- src/LearningAI.Api/RequestHandlers/DocumentAssistantQueryRequestHandler.cs
+++ src/LearningAI.Api/RequestHandlers/DocumentAssistantQueryRequestHandler.cs
@@ -27,36 +27,10 @@ public class DocumentAssistantQueryRequestHandler(
     {
         string? assistantResponse = null;
         var inputKind = await DetermineInputKindAsync(request, cancellationToken);
+
         if (inputKind == InputKind.Prompt)
         {
-            var messages = new List<ChatMessage>
-            {
-                new(
-                    ChatRole.System,
-                    """
-                    You are an assistant who answers questions by finding and compiling the requested information from a company's internal knowledgebase.
-
-                    Use the tools available to you to search for documents where you may find relevant information or to get any additional required data.
-
-                    Don't make up information, politely reply that you don't know the answer if you cannot find any information on what the user is asking.
-
-                    Your answer should:
-                    - be in markdown format
-                    - contain URLs which you created that point to the documents you used in the answer.
-                    """),
-                new(ChatRole.User, request.Query),
-            };
-
-            // TODO: Play around with what to do if the references are too large. Ideas:
-            // - Detect when there are too many tokens and only do the search. Return a simple response saying that the documents are too large but
-            //   here are the relevant documents to read more: <link>
-            // - In a separate LLM conversation for each search result, ask if it contains info on what's being asked. If so, ask to summarize
-            //   it/create an extract if the doc contains other unrelated topics and send the shortened form to the main conversation
-            var aiResponse = await chatClient.GetResponseAsync(messages, CreateChatOptions(), cancellationToken);
-
-            assistantResponse = string.IsNullOrEmpty(aiResponse.Text)
-                ? "[Warning] No response received from assistant."
-                : aiResponse.Text;
+            assistantResponse = await GetAssistantResponseAsync(request, assistantResponse, cancellationToken);
         }
 
         var searchTerms = await GetSearchTermsAsync(request, inputKind, cancellationToken);
@@ -71,20 +45,50 @@ public class DocumentAssistantQueryRequestHandler(
                     .ToList()));
     }
 
+    private async Task<string> GetAssistantResponseAsync(QueryAssistantRequest request, string? assistantResponse, CancellationToken cancellationToken)
+    {
+        var messages = new List<ChatMessage>
+        {
+            new(
+                ChatRole.System,
+                """
+                You are an assistant who answers questions by finding and compiling the requested information from a company's internal knowledgebase.
+
+                Use the tools available to you to search for documents where you may find relevant information or to get any additional required data.
+
+                Don't make up information, politely reply that you don't know the answer if you cannot find any information on what the user is asking.
+
+                Your answer should:
+                - be in markdown format
+                - contain URLs which you created that point to the documents you used in the answer.
+                """),
+            new(ChatRole.User, request.Query),
+        };
+
+        // TODO: Play around with what to do if the references are too large. Ideas:
+        // - Detect when there are too many tokens and only do the search. Return a simple response saying that the documents are too large but
+        //   here are the relevant documents to read more: <link>
+        // - In a separate LLM conversation for each search result, ask if it contains info on what's being asked. If so, ask to summarize
+        //   it/create an extract if the doc contains other unrelated topics and send the shortened form to the main conversation
+        var aiResponse = await chatClient.GetResponseAsync(messages, CreateChatOptions(), cancellationToken);
+
+        return string.IsNullOrEmpty(aiResponse.Text)
+            ? "[Warning] No response received from assistant."
+            : aiResponse.Text;
+    }
+
     private async Task<IReadOnlyCollection<string>> GetSearchTermsAsync(
         QueryAssistantRequest request,
         InputKind inputKind,
         CancellationToken cancellationToken)
     {
-        var regexTimeout = TimeSpan.FromSeconds(1);
-
         const string instructionsForPrompt =
             """
             Consider the following prompt entered by a user on a company knowledgebase page.
             Create up to 10 terms-based queries derived from the prompt which have similar meaning to the prompt
             but only contains the key terms and phrases or their synonyms from the input.
             """;
-        const string instructionsForTermsQuery = "" +
+        const string instructionsForTermsQuery =
             """
             Consider the following terms-based query entered by a user on a company knowledgebase page.
             Create up to 10 alternative terms-based queries derived from the input which have similar meaning to the original
@@ -96,6 +100,7 @@ public class DocumentAssistantQueryRequestHandler(
             return [request.Query];
         }
 
+        var regexTimeout = TimeSpan.FromSeconds(1);
         var messages = new List<ChatMessage>
         {
             new(ChatRole.System, inputKind == InputKind.TermsQuery ? instructionsForTermsQuery : instructionsForPrompt),
